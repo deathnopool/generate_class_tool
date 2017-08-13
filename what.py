c7afad1280c9c5d6facd20b00d9023a855ffa361
fuckoffException <1946958405@qq.com>
2017-08-14 00:47:32 +0800

    变量直接从数据库读取
    变量直接从数据库读取

--- what.py
+++ what.py
@@ -2,11 +2,34 @@
 # -*- coding:utf-8 -*-
 import os
 from string import Template 
+import psycopg2
+import json
 
-className = 'Category'
-fieldNames = [{'id': "number"}, {'parentId': "number"}, {'name': "string"}, {"description": "string"},{"commonId": "number"},{'creatorId': "number"}, {'createTime': "number"}, {"updatorId": 'number'}, {'updateTime': 'number'}, {'syncTime': 'number'}, {'disabled': 'number'}]
+configFile = open('config.json', 'r')
+config = json.load(configFile, encoding='utf-8')
+
+DATABASE = config['database']
+USER = config['user']
+PASSWORD = config['password']
+HOST = config['host']
+PORT = config['port']
+SCHEMA = config['schema']
+TABLE = config['table']
+CLASS = config['class']
+EXTENSION = config['extension']
+
+conn = psycopg2.connect(database=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT)
+print 'connecting to database successful!'
+cur = conn.cursor()
+cur.execute('select COLUMN_NAME, DATA_TYPE from information_schema.columns where table_schema=\''+SCHEMA +'\' and table_name=\''+TABLE+ '\';')
+rows = cur.fetchall()
+conn.close()
+
+className = CLASS
+fieldNames = rows
+
 destDir = './'+className
-classFileName = destDir + '/' + className + ".server.ts"
+classFileName = destDir + '/' + className + EXTENSION
 methodsCode = ''
 classCode = ''
 interfaceCode = ''
@@ -16,10 +39,17 @@ fieldTempt = Template(f.read())
 interfaceTempt = Template('    ${field}: ${type},\n')
 classTempt= ''
 
+def translateType(t):
+    if t == 'bigint' or t == 'integer':
+        return 'number'
+    elif t == 'character varying' or t == 'text':
+        return 'string'
+    return 'object'
+
 for field in fieldNames:
-    fName = field.keys()[0]
+    fName = field[0]
     FName = fName[0].upper() + fName[1: len(fName)]
-    tName = field.values()[0]
+    tName = translateType(field[1])
     methodsCode += '\n' + fieldTempt.substitute(field=fName, type=tName, Field=FName)
     interfaceCode += interfaceTempt.substitute(field=fName, type=tName)
 f = open('./class.shit', 'r')
